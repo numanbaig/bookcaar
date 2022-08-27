26ba9d537432f0ce4391de7fb22dc0fe853ca01a
Noman <numansafi97@gmail.com>
2022-08-27 16:07:47 +0500

    feat: configure firebase

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,20 @@ import App from './App'
 import './index.css'
 import { Provider } from "react-redux"
 import { store } from "./store"
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.REACT_APP_MEASUREMENT_ID
+};
+
+initializeApp(firebaseConfig);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
