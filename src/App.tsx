1dfc65fca498bb4dd5378aabc1ed10434eae5831
salman karim <pinesalman@gmail.com>
2022-08-27 19:34:54 +0500

    route issue fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { AppRoutes } from "routes";
+import { AppRoutes } from "./routes";
 
 function App() {
   return <AppRoutes />;
