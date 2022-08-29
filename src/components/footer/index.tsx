321f396aece41f839795b66ee7bcd2baad6c482b
shahrukh khan <ikhan.sharu977@gmail.com>
2022-08-29 11:09:00 +0500

    feat:footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './index.css'
+
+function index() {
+  return (
+<section className="footer">
+
+    {/* <div className="box-container">
+
+        <div className="box">
+            <a href="#" className="logo"> <i className="fas fa-paper-plane"></i>travel </a>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, ad?</p>
+            <div className="share">
+                <a href="#" className="fab fa-facebook-f"></a>
+                <a href="#" className="fab fa-twitter"></a>
+                <a href="#" className="fab fa-instagram"></a>
+                <a href="#" className="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div className="box" data-aos="fade-up" data-aos-delay="300">
+            <h3>quick links</h3>
+            <a href="#home" className="links"> <i className="fas fa-arrow-right"></i> home </a>
+            <a href="#about" className="links"> <i className="fas fa-arrow-right"></i> about </a>
+            <a href="#destination" className="links"> <i className="fas fa-arrow-right"></i> destination </a>
+            <a href="#services" className="links"> <i className="fas fa-arrow-right"></i> services </a>
+            <a href="#gallery" className="links"> <i className="fas fa-arrow-right"></i> gallery </a>
+            <a href="#blogs" className="links"> <i className="fas fa-arrow-right"></i> blogs </a>
+        </div>
+
+        <div className="box" data-aos="fade-up" data-aos-delay="450">
+            <h3>contact info</h3>
+            <p> <i className="fas fa-map"></i> mumbai, india </p>
+            <p> <i className="fas fa-phone"></i> +123-456-7890 </p>
+            <p> <i className="fas fa-envelope"></i> shaikhanas@gmail.com </p>
+            <p> <i className="fas fa-clock"></i> 7:00am - 10:00pm </p>
+        </div>
+
+        <div className="box" data-aos="fade-up" data-aos-delay="600">
+            <h3>newsletter</h3>
+            <p>subscribe for latest updates</p>
+            <div>
+                <input type="email" name="" placeholder="enter your email" className="email" id=""/>
+                <input type="submit" value="subscribe" className="btn"/>
+            </div>
+        </div>
+
+    </div> */}
+
+</section>
+  )
+}
+
+export default index
